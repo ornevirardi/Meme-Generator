1d69891c7c166be77559ec87dd6fdc7147769d0e
Orne Virardi <73016255+ornevirardi@users.noreply.github.com>
2022-05-25 09:49:28 -0700

    Update Meme.js

--- components/Meme.js
+++ components/Meme.js
@@ -8,17 +8,15 @@ export default function Meme() {
     })
     const [allMemes, setAllMemes] = React.useState([])
     
-
     
     React.useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(data => setAllMemes(data.data.memes))
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
     }, [])
-    
-    
-    
-    
     
     function getMemeImage() {
         const randomNumber = Math.floor(Math.random() * allMemes.length)
@@ -27,7 +25,6 @@ export default function Meme() {
             ...prevMeme,
             randomImage: url
         }))
-        
     }
     
     function handleChange(event) {
